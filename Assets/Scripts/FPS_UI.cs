85ddcfc92968988e4ddde06ccd64ffb00e58a06d
Shem Tom <63319003+shemtomke@users.noreply.github.com>
2023-01-12 02:04:55 +0300

    update

---
+++ Assets/Scripts/FPS_UI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS_UI : MonoBehaviour
+{
+    public Text fpsCounter;
+
+    private void Update()
+    {
+        
+    }
+}
